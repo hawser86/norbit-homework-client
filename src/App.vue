a4e73da345f55ad148f80743347506f9398aa280
hawser <hawser86@gmail.com>
2023-11-21 14:35:19 +0100

    show a map

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,44 @@
 <script>
+import Map from './components/Map.vue';
+
+export default {
+  name: 'App',
+  components: { Map }
+}
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <div class="box">
+    <div class="row header">
+      <h1>Boat position</h1>
+    </div>
+
+    <div class="row content">
+      <Map></Map>
+    </div>
+
+    <div class="row footer">
+      footer...
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.box {
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+}
+
+.box .row.header {
+  flex: 0 1 auto;
+}
+
+.box .row.content {
+  flex: 1 1 auto;
+}
+
+.box .row.footer {
+  flex: 0 1 40px;
+}
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,34 @@
+<script>
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
+
+export default {
+  name: "Map",
+  mounted() {
+    new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: fromLonLat([19.042563, 47.512841]),
+        zoom: 13
+      })
+    });
+  }
+}
+</script>
+
+<template>
+  <div id="map"></div>
+</template>
+
+<style scoped>
+  #map {
+    height: 100%;
+  }
+</style>
